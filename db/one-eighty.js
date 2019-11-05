ffb19a9d721b60593eac6d79ad22fc0cee40e333
eric <cirept612@gmail.com>
2019-11-05 11:47:20 -0800

    created mock data for all project types

---
+++ db/one-eighty.js
@@ -0,0 +1,131 @@
+const oneEighty = [
+    {
+        "id": 0,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "2",
+        "workzone": "Canada",
+        "match": true
+    },
+    {
+        "id": 1,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "3",
+        "workzone": "Western"
+    },
+    {
+        "id": 2,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "2",
+        "workzone": "Central"
+    },
+    {
+        "id": 3,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "5",
+        "workzone": "North"
+    },
+    {
+        "id": 4,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "2",
+        "workzone": "South"
+    },
+    {
+        "id": 5,
+        "week": "8/12/2019 - 8/18/2019",
+        "resources": "2",
+        "workzone": "West"
+    },
+    {
+        "id": 6,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "2",
+        "workzone": "Canada"
+    },
+    {
+        "id": 7,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "2",
+        "workzone": "Western"
+    },
+    {
+        "id": 8,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "2",
+        "workzone": "Western"
+    },
+    {
+        "id": 9,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "2",
+        "workzone": "Central"
+    },
+    {
+        "id": 10,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "1",
+        "workzone": "North"
+    },
+    {
+        "id": 11,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "2",
+        "workzone": "South"
+    },
+    {
+        "id": 12,
+        "week": "8/19/2019 - 8/25/2019",
+        "resources": "1",
+        "workzone": "West"
+    },
+    {
+        "id": 13,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "Canada"
+    },
+    {
+        "id": 14,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "Western"
+    },
+    {
+        "id": 15,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "Western"
+    },
+    {
+        "id": 16,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "Central"
+    },
+    {
+        "id": 17,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "1",
+        "workzone": "North"
+    },
+    {
+        "id": 18,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "South"
+    },
+    {
+        "id": 19,
+        "week": "8/26/2019 - 9/1/2019",
+        "resources": "2",
+        "workzone": "West"
+    },
+    {
+        "id": 20,
+        "week": "9/2/2019 - 9/8/2019",
+        "resources": "2",
+        "workzone": "Canada"
+    }
+];
+
+export default oneEighty;
